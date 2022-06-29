fe3df302fd535ac37c2f00f9ba6f0a54c6d7274f
elfnor <elfnor@users.noreply.github.com>
2022-06-29 14:48:49 +1200

    Update LS15.bat

--- LS15.bat
+++ LS15.bat
@@ -1,3 +1,3 @@
 @echo on
 call %USERPROFILE%\Miniconda3\Scripts\activate.bat
-%USERPROFILE%\Miniconda3\python.exe "C:\Users\Public\ls_digital_level\LS15_bluetooth.py
+%USERPROFILE%\Miniconda3\python.exe "C:\Users\Public\ls_digital_level\LS15_bluetooth.py"
